8296ff82b2a5ed0f142db679372d5db2d527c894
Steve Stevens <steve.h.i.stevens@gmail.com>
2019-03-08 23:53:53 +0000

    parse sample backup report to capture starttime

--- src/RoboCopyReport.py
+++ src/RoboCopyReport.py
@@ -1,16 +1,25 @@
-#!/usr/bin/python
-
 import re
-import datetime
+from datetime import datetime as dt
+
+fmt_datetime = '%a %b %d %H:%M:%S %Y'
+
+r_started = r'.*Started : (.*)'
+
+
+def _regex_it(string, regex):
+    return re.search(regex, string) \
+        .group(1) \
+        .lstrip().rstrip()
 
 
 class RoboCopyReport(object):
     def __init__(self, raw_report):
-        self.raw_report
+        self.raw_report = raw_report
         self._parse_raw_report(raw_report)
 
     def _parse_raw_report(self, raw_report):
-        self.datetime_start = datetime.now()
+        start_str = _regex_it(raw_report, r_started)
+        self.datetime_start = dt.strptime(start_str, fmt_datetime)
 
     def get_start_time(self):
         return self.datetime_start
--- tst/test_RoboCopyReport.py
+++ tst/test_RoboCopyReport.py
@@ -1,6 +1,14 @@
 from src.RoboCopyReport import *
+from os import path
+from datetime import datetime as dt
 
 
 class TestRoboCopyReport:
+    def __init__(self):
+        pass
+
     def test_can_parse_report_start_time(self):
-        assert 1 == 1
+        raw_report = open(path.join("_test_files", "no_fault")).read()
+        actual = RoboCopyReport(raw_report).get_start_time()
+        expected = dt(2019, 3, 8, 0, 5, 0)
+        assert actual.ctime() == expected.ctime()
