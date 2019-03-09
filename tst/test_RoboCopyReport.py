144065fda311947a9cadfbeadec06b913ac34773
Steve Stevens <steve.h.i.stevens@gmail.com>
2019-03-09 00:49:52 +0000

    refactor function for getting test files

--- tst/test_RoboCopyReport.py
+++ tst/test_RoboCopyReport.py
@@ -5,9 +5,13 @@ from datetime import datetime as dt
 dir_path = path.dirname(path.realpath(__file__))
 
 
+def _get_test_file(file_name):
+    return open(path.join(dir_path, "_test_files", file_name)).read()
+
+
 class TestRoboCopyReport:
     def test_can_parse_report_start_time(self):
-        raw_report = open(path.join(dir_path, "_test_files", "failed_false")).read()
+        raw_report = _get_test_file("failed_false")
 
         actual = RoboCopyReport(raw_report).get_start_time()
         expected = dt(2019, 3, 8, 0, 5, 0)
@@ -15,7 +19,7 @@ class TestRoboCopyReport:
         assert actual.ctime() == expected.ctime()
 
     def test_can_parse_report_end_time(self):
-        raw_report = open(path.join(dir_path, "_test_files", "failed_false")).read()
+        raw_report = _get_test_file("failed_false")
 
         actual = RoboCopyReport(raw_report).get_end_time()
         expected = dt(2019, 3, 8, 0, 5, 1)
@@ -23,7 +27,7 @@ class TestRoboCopyReport:
         assert actual.ctime() == expected.ctime()
 
     def test_can_get_failed_dirs(self):
-        raw_report = open(path.join(dir_path, "_test_files", "failed_true")).read()
+        raw_report = _get_test_file("failed_true")
 
         actual = RoboCopyReport(raw_report).get_failed_dirs()
         expected = 2
@@ -31,7 +35,7 @@ class TestRoboCopyReport:
         assert actual == expected
 
     def test_can_get_failed_files(self):
-        raw_report = open(path.join(dir_path, "_test_files", "failed_true")).read()
+        raw_report = _get_test_file("failed_true")
 
         actual = RoboCopyReport(raw_report).get_failed_files()
         expected = 5
@@ -39,14 +43,14 @@ class TestRoboCopyReport:
         assert actual == expected
 
     def test_can_get_failed_true(self):
-        raw_report = open(path.join(dir_path, "_test_files", "failed_true")).read()
+        raw_report = _get_test_file("failed_true")
 
         actual = RoboCopyReport(raw_report)
 
         assert actual.has_failed() == True
 
     def test_can_get_failed_false(self):
-        raw_report = open(path.join(dir_path, "_test_files", "failed_false")).read()
+        raw_report = _get_test_file("failed_false")
 
         actual = RoboCopyReport(raw_report)
 
