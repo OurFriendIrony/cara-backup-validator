2175027ccb5534010eb44737c9dde401f79c0202
Steve Stevens <steve.h.i.stevens@gmail.com>
2019-03-09 00:04:50 +0000

    parse enddate

--- src/RoboCopyReport.py
+++ src/RoboCopyReport.py
@@ -3,8 +3,6 @@ from datetime import datetime as dt
 
 fmt_datetime = '%a %b %d %H:%M:%S %Y'
 
-r_started = r'.*Started : (.*)'
-
 
 def _regex_it(string, regex):
     return re.search(regex, string) \
@@ -12,14 +10,18 @@ def _regex_it(string, regex):
         .lstrip().rstrip()
 
 
+def _regex_it_datetime(raw_report, regex):
+    start_str = _regex_it(raw_report, regex)
+    return dt.strptime(start_str, fmt_datetime)
+
+
 class RoboCopyReport(object):
     def __init__(self, raw_report):
-        self.raw_report = raw_report
-        self._parse_raw_report(raw_report)
-
-    def _parse_raw_report(self, raw_report):
-        start_str = _regex_it(raw_report, r_started)
-        self.datetime_start = dt.strptime(start_str, fmt_datetime)
+        self.datetime_start = _regex_it_datetime(raw_report, r'.*Started : (.*)')
+        self.datetime_end = _regex_it_datetime(raw_report, r'.*Ended : (.*)')
 
     def get_start_time(self):
         return self.datetime_start
+
+    def get_end_time(self):
+        return self.datetime_end
--- tst/test_RoboCopyReport.py
+++ tst/test_RoboCopyReport.py
@@ -2,13 +2,18 @@ from src.RoboCopyReport import *
 from os import path
 from datetime import datetime as dt
 
+dir_path = path.dirname(path.realpath(__file__))
+
 
 class TestRoboCopyReport:
-    def __init__(self):
-        pass
-
     def test_can_parse_report_start_time(self):
-        raw_report = open(path.join("_test_files", "no_fault")).read()
+        raw_report = open(path.join(dir_path, "_test_files", "no_fault")).read()
         actual = RoboCopyReport(raw_report).get_start_time()
         expected = dt(2019, 3, 8, 0, 5, 0)
+        assert actual.ctime() == expected.ctime()
+
+    def test_can_parse_report_end_time(self):
+        raw_report = open(path.join(dir_path, "_test_files", "no_fault")).read()
+        actual = RoboCopyReport(raw_report).get_end_time()
+        expected = dt(2019, 3, 8, 0, 5, 1)
         assert actual.ctime() == expected.ctime()
