0c05ee0bd379045ca3e14ce85b49c5086e122b51
Steve Stevens <steve.h.i.stevens@gmail.com>
2019-03-09 00:35:38 +0000

    parsed failed dir and files count

--- src/RoboCopyReport.py
+++ src/RoboCopyReport.py
@@ -1,27 +1,42 @@
 import re
 from datetime import datetime as dt
 
-fmt_datetime = '%a %b %d %H:%M:%S %Y'
+R_STARTED = r'.*Started : (.*)'
+R_ENDED = r'.*Ended : (.*)'
+R_DIRS = r'Dirs : \s*(\d+)\s*(\d+)\s*(\d+)\s*(\d+)\s*(\d+)\s*(\d+)'
+R_FILES = r'Files : \s*(\d+)\s*(\d+)\s*(\d+)\s*(\d+)\s*(\d+)\s*(\d+)'
+
+FMT_DT = '%a %b %d %H:%M:%S %Y'
 
 
-def _regex_it(string, regex):
+def _regex_it(string, regex, group=1):
+    print regex
+    print string
     return re.search(regex, string) \
-        .group(1) \
+        .group(group) \
         .lstrip().rstrip()
 
 
 def _regex_it_datetime(raw_report, regex):
     start_str = _regex_it(raw_report, regex)
-    return dt.strptime(start_str, fmt_datetime)
+    return dt.strptime(start_str, FMT_DT)
 
 
 class RoboCopyReport(object):
     def __init__(self, raw_report):
-        self.datetime_start = _regex_it_datetime(raw_report, r'.*Started : (.*)')
-        self.datetime_end = _regex_it_datetime(raw_report, r'.*Ended : (.*)')
+        self.datetime_start = _regex_it_datetime(raw_report, R_STARTED)
+        self.datetime_end = _regex_it_datetime(raw_report, R_ENDED)
+        self.failed_dirs = int(_regex_it(raw_report, R_DIRS, group=5))
+        self.failed_files = int(_regex_it(raw_report, R_FILES, group=5))
 
     def get_start_time(self):
         return self.datetime_start
 
     def get_end_time(self):
         return self.datetime_end
+
+    def get_failed_dirs(self):
+        return self.failed_dirs
+
+    def get_failed_files(self):
+        return self.failed_files
--- tst/test_RoboCopyReport.py
+++ tst/test_RoboCopyReport.py
@@ -7,13 +7,33 @@ dir_path = path.dirname(path.realpath(__file__))
 
 class TestRoboCopyReport:
     def test_can_parse_report_start_time(self):
-        raw_report = open(path.join(dir_path, "_test_files", "no_fault")).read()
+        raw_report = open(path.join(dir_path, "_test_files", "failed_false")).read()
+
         actual = RoboCopyReport(raw_report).get_start_time()
         expected = dt(2019, 3, 8, 0, 5, 0)
+
         assert actual.ctime() == expected.ctime()
 
     def test_can_parse_report_end_time(self):
-        raw_report = open(path.join(dir_path, "_test_files", "no_fault")).read()
+        raw_report = open(path.join(dir_path, "_test_files", "failed_false")).read()
+
         actual = RoboCopyReport(raw_report).get_end_time()
         expected = dt(2019, 3, 8, 0, 5, 1)
+
         assert actual.ctime() == expected.ctime()
+
+    def test_can_get_failed_dirs(self):
+        raw_report = open(path.join(dir_path, "_test_files", "failed_true")).read()
+
+        actual = RoboCopyReport(raw_report).get_failed_dirs()
+        expected = 2
+
+        assert actual == expected
+
+    def test_can_get_failed_files(self):
+        raw_report = open(path.join(dir_path, "_test_files", "failed_true")).read()
+
+        actual = RoboCopyReport(raw_report).get_failed_files()
+        expected = 5
+
+        assert actual == expected
