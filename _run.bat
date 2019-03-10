82bf510877010a9e58a7f4792cd6fb50321a6f5a
OurFriendIrony <OurFriendIrony@users.noreply.github.com>
2019-03-10 11:59:00 +0000

    Update _run.bat

--- _run.bat
+++ _run.bat
@@ -1,3 +1,9 @@
-ECHO ON
-C:\Python27 src\RoboCopyValidator.py
-PAUSE
+REM Setup Environment
+    set PYTHONHOME=C:\Python27
+    set PYTHONPATH=%PYTHONHOME%\Lib;.
+    set PATH=%PYTHONHOME%;%PATH%
+
+REM Execute Validator
+    ECHO ON
+    python src\RoboCopyValidator.py
+    PAUSE
